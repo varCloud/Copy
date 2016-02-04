e4a5b107217ea3da99a15e00057fdaee6ca6c9cf
Briseida Atziri Zaragoza Salas <Brisa@MacBook-Pro-de-Briseida.local>
2016-02-03 22:09:34 -0600

    ya quedo la base

--- Copy/app/src/main/java/com/example/brisa/copy/Conexion.java
+++ Copy/app/src/main/java/com/example/brisa/copy/Conexion.java
@@ -16,7 +16,7 @@ public class Conexion extends SQLiteOpenHelper {
     private  Conexion conexion;
     private static final String __DATABASE = "dbCopy";
     //versión de la base de datos
-    private static final int __VERSION = 5;
+    private static final int __VERSION = 17;
     //nombre tabla y campos de tabla
     public final String __tabla__ = "persona";
     public final String CatTipoCopias = "CatTipoCopias";
@@ -57,11 +57,7 @@ public class Conexion extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.d("el script actual es ",bd);
-        db.execSQL(bd);
-        db.execSQL(tablaCatTipoCopias);
-        db.execSQL(tablaVentas);
-        db.execSQL(tablaRegistroVentas);
+        CreaBase(db);
     }
 
     @Override
@@ -69,16 +65,23 @@ public class Conexion extends SQLiteOpenHelper {
         if ( newVersion > oldVersion )
         {
             //elimina tabla
-            db.execSQL( "DROP TABLE IF EXISTS "+CatTipoCopias);
-            db.execSQL( "DROP TABLE IF EXISTS "+ Ventas);
-            db.execSQL( "DROP TABLE IF EXISTS "+TotalVentas);
+            db.execSQL( "DROP TABLE IF EXISTS catporcentaje");
+            db.execSQL("DROP TABLE IF EXISTS cattipocopia");
+            db.execSQL( "DROP TABLE IF EXISTS detallesventa");
+            db.execSQL( "DROP TABLE IF EXISTS rangosxtipocopiabn");
+            db.execSQL( "DROP TABLE IF EXISTS porcentajexcopiacolor");
+            db.execSQL( "DROP TABLE IF EXISTS detallesventa");
+            db.execSQL( "DROP TABLE IF EXISTS rangosxtipocopiacolor");
+            db.execSQL( "DROP TABLE IF EXISTS ventas");
+            CreaBase(db);
+
             //y luego creamos la nueva tabla
-            db.execSQL(tablaCatTipoCopias);
+            /*db.execSQL(tablaCatTipoCopias);
             db.execSQL(tablaVentas);
             db.execSQL(tablaRegistroVentas);
 
-            db.execSQL("INSERT INTO " + conexion.CatTipoCopias + " values (1,'color',3.2)");
-            db.execSQL("INSERT INTO " + conexion.CatTipoCopias + " values (2,'Blanco / Negro',2.5)");
+            db.execSQL("INSERT INTO " + conexion.CatTipoCopias + " values (1,color,3.2)");
+            db.execSQL("INSERT INTO " + conexion.CatTipoCopias + " values (2,Blanco / Negro,2.5)");*/
         }
     }
 
@@ -88,145 +91,118 @@ public class Conexion extends SQLiteOpenHelper {
 
     }
 
-    public final String bd = "  CREATE TABLE catporcentaje (\n" +
-            "  idPorcentaje INTEGER AUTOINCREMENT NOT NULL ,\n" +
-            "  descripcion varchar(100) NOT NULL\n" +
-            ")\n -- ----------------------------\n" +
-            "-- Records of catporcentaje\n" +
-            "-- ----------------------------\n" +
-            "INSERT INTO catporcentaje VALUES ('1', '20%');\n" +
-            "INSERT INTO catporcentaje VALUES ('2', '50%');\n" +
-            "INSERT INTO catporcentaje VALUES ('3', '100%');\n" +
-            "CREATE TABLE cattipocopia (\n" +
-            "  idCatTipoCopia int(11) INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-            "  Descripcion varchar(100) NOT NULL )\n"+
-            "-- ----------------------------\n" +
-            "-- Records of cattipocopia\n" +
-            "-- ----------------------------\n" +
-            "INSERT INTO cattipocopia VALUES ('1', 'Carta');\n" +
-            "INSERT INTO cattipocopia VALUES ('2', 'Oficio');\n" +
-            "INSERT INTO cattipocopia VALUES ('3', 'Doble Carta');\n" +
-            "\n" +"-- ----------------------------\n" +
-            "-- Table structure for detallesventa\n" +
-            "-- ----------------------------\n" +
-            "CREATE TABLE detallesventa (\n" +
-            "  idDetalleVenta int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-            "  idVenta int(11) NOT NULL,\n" +
-            "  idRangoXTipoCopiaBN int(11) DEFAULT NULL,\n" +
-            "  cantidadCopiasBN int(11) DEFAULT NULL,\n" +
-            "  costoCopiasBN decimal(10,2) DEFAULT NULL,\n" +
-            "  idCatTipoCopia int(11) DEFAULT NULL,\n" +
-            "  idPorcentajeCopiaColor int(11) DEFAULT NULL,\n" +
-            "  cantidadCopiasColor int(11) DEFAULT NULL,\n" +
-            "  costoCopiasColor decimal(10,2) DEFAULT NULL,\n" +
-            "-- ----------------------------\n" +
-            "-- Records of detallesventa\n" +
-            "-- ----------------------------\n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Table structure for porcentajexcopiacolor\n" +
-            "-- ----------------------------\n" +
-            "CREATE TABLE porcentajexcopiacolor (\n" +
-            "  idPorcentajeCopiaColor int(11) PRIMARY KEY NOT NULL AUTOINCREMENT,\n" +
-            "  idRangoXTipoCopiaColor int(11) NOT NULL,\n" +
-            "  IdPorcentaje int(11) NOT NULL,\n" +
-            "  costo decimal(10,2) NOT NULL,\n" +
-            ") \n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Records of porcentajexcopiacolor\n" +
-            "-- ----------------------------\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('1', '1', '1', '2.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('2', '1', '2', '3.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('3', '1', '3', '5.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('4', '2', '1', '1.80');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('5', '2', '2', '2.70');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('6', '2', '3', '4.50');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('7', '3', '1', '1.50');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('8', '3', '2', '2.40');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('9', '3', '3', '4.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('10', '4', '1', '3.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('11', '4', '2', '4.50');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('12', '4', '3', '7.50');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('13', '5', '1', '2.70');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('14', '5', '2', '4.05');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('15', '5', '3', '6.75');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('16', '6', '1', '2.40');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('17', '6', '2', '3.60');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('18', '6', '3', '6.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('19', '7', '1', '4.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('20', '7', '2', '6.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('21', '7', '3', '10.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('22', '8', '1', '3.60');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('23', '8', '2', '5.40');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('24', '8', '3', '9.00');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('25', '9', '1', '3.20');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('26', '9', '2', '4.80');\n" +
-            "INSERT INTO porcentajexcopiacolor VALUES ('27', '9', '3', '8.00');\n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Table structure for rangosxtipocopiabn\n" +
-            "-- ----------------------------\n" +
-            "CREATE TABLE rangosxtipocopiabn (\n" +
-            "  idRangoXTipoCopiaBN int(11) PRIMARY KEY NOT NULL AUTOINCREMENT,\n" +
-            "  min int(11) DEFAULT NULL,\n" +
-            "  max int(11) DEFAULT NULL,\n" +
-            "  costo decimal(10,2) DEFAULT NULL,\n" +
-            "  idCatTipoCopia int(11) DEFAULT NULL,\n" +
-            ") \n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Records of rangosxtipocopiabn\n" +
-            "-- ----------------------------\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('1', '1', '5', '1.00', '1');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('2', '6', '99', '0.50', '1');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('3', '100', '999', '0.35', '1');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('4', '1000', '99999', '0.30', '1');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('5', '1', '5', '1.50', '2');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('6', '6', '99', '0.75', '2');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('7', '100', '999', '0.53', '2');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('8', '1000', '99999', '0.45', '2');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('9', '1', '5', '2.00', '3');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('10', '6', '99', '1.00', '3');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('11', '100', '999', '0.70', '3');\n" +
-            "INSERT INTO rangosxtipocopiabn VALUES ('12', '1000', '99999', '0.60', '3');\n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Table structure for rangosxtipocopiacolor\n" +
-            "-- ----------------------------\n" +
-            "DROP TABLE IF EXISTS rangosxtipocopiacolor;\n" +
-            "CREATE TABLE rangosxtipocopiacolor (\n" +
-            "  idRangoXTipoCopiaColor int(11) PRIMARY KEY NOT NULL AUTOINCREMENT,\n" +
-            "  min int(11) DEFAULT NULL,\n" +
-            "  max int(11) DEFAULT NULL,\n" +
-            "  idCatTipoCopia int(11) NOT NULL,\n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Records of rangosxtipocopiacolor\n" +
-            "-- ----------------------------\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('1', '1', '24', '1');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('2', '10', '49', '1');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('3', '50', '99999', '1');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('4', '1', '24', '2');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('5', '10', '49', '2');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('6', '50', '99999', '2');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('7', '1', '24', '3');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('8', '10', '49', '3');\n" +
-            "INSERT INTO rangosxtipocopiacolor VALUES ('9', '50', '99999', '3');\n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Table structure for ventas\n" +
-            "-- ----------------------------\n" +
-            "CREATE TABLE ventas (\n" +
-            "  idVenta int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-            "  fechaVenta datetime NOT NULL,\n" +
-            "  cantidadCopiasTotal int(11) NOT NULL,\n" +
-            "  Total decimal(10,2) NOT NULL,\n" +
-            ") \n" +
-            "\n" +
-            "-- ----------------------------\n" +
-            "-- Records of ventas\n" +
-            "-- ----------------------------\n";
+    public void CreaBase(SQLiteDatabase db)
+    {
+ try {
+     db.execSQL("CREATE TABLE catporcentaje (" +
+             "  idPorcentaje INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  descripcion varchar(100) NOT NULL)");
+
+     db.execSQL("INSERT INTO catporcentaje VALUES (1, '20%')");
+     db.execSQL("INSERT INTO catporcentaje VALUES (2, '50%')");
+     db.execSQL("INSERT INTO catporcentaje VALUES (3, '100%')");
+
+
+     db.execSQL("CREATE TABLE cattipocopia (\n" +
+             "  idCatTipoCopia INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  Descripcion TEXT NOT NULL )");
+
+     db.execSQL("INSERT INTO cattipocopia VALUES (1, 'Carta')");
+     db.execSQL("INSERT INTO cattipocopia VALUES (2, 'Oficio')");
+     db.execSQL("INSERT INTO cattipocopia VALUES (3, 'Doble Carta')");
+
+     db.execSQL("CREATE TABLE detallesventa (\n" +
+             "  idDetalleVenta INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  idVenta INTEGER NOT NULL,\n" +
+             "  idRangoXTipoCopiaBN INTEGER ,\n" +
+             "  cantidadCopiasBN INTEGER ,\n" +
+             "  costoCopiasBN NUMERIC ,\n" +
+             "  idCatTipoCopia INTEGER ,\n" +
+             "  idPorcentajeCopiaColor INTEGER ,\n" +
+             "  cantidadCopiasColor INTEGER ,\n" +
+             "  costoCopiasColor NUMERIC )");
+
+     db.execSQL("CREATE TABLE porcentajexcopiacolor (\n" +
+             "  idPorcentajeCopiaColor INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  idRangoXTipoCopiaColor INTEGER NOT NULL,\n" +
+             "  IdPorcentaje INTEGER NOT NULL,\n" +
+             "  costo NUMERIC NOT NULL)");
+
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (1, 1, 1, 2.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (2, 1, 2, 3.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (3, 1, 3, 5.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (4, 2, 1, 1.80)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (5, 2, 2, 2.70)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (6, 2, 3, 4.50)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (7, 3, 1, 1.50)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (8, 3, 2, 2.40)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (9, 3, 3, 4.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (10, 4, 1, 3.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (11, 4, 2, 4.50)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (12, 4, 3, 7.50)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (13, 5, 1, 2.70)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (14, 5, 2, 4.05)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (15, 5, 3, 6.75)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (16, 6, 1, 2.40)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (17, 6, 2, 3.60)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (18, 6, 3, 6.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (19, 7, 1, 4.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (20, 7, 2, 6.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (21, 7, 3, 10.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (22, 8, 1, 3.60)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (23, 8, 2, 5.40)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (24, 8, 3, 9.00)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (25, 9, 1, 3.20)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (26, 9, 2, 4.80)");
+     db.execSQL(" INSERT INTO porcentajexcopiacolor VALUES (27, 9, 3, 8.00)");
+
+     db.execSQL("CREATE TABLE rangosxtipocopiabn (\n" +
+             "  idRangoXTipoCopiaBN INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  min INTEGER ,\n" +
+             "  max INTEGER ,\n" +
+             "  costo NUMERIC ,\n" +
+             "  idCatTipoCopia INTEGER )");
+
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (1, 1, 5, 1.00, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (2, 6, 99, 0.50, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (3, 100, 999, 0.35, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (4, 1000, 99999, 0.30, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (5, 1, 5, 1.50, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (6, 6, 99, 0.75, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (7, 100, 999, 0.53, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (8, 1000, 99999, 0.45, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (9, 1, 5, 2.00, 3)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (10, 6, 99, 1.00, 3)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (11, 100, 999, 0.70, 3)");
+     db.execSQL(" INSERT INTO rangosxtipocopiabn VALUES (12, 1000, 99999, 0.60, 3)");
+
+     db.execSQL("CREATE TABLE rangosxtipocopiacolor (\n" +
+             "  idRangoXTipoCopiaColor INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  min INTEGER ,\n" +
+             "  max INTEGER ,\n" +
+             "  idCatTipoCopia INTEGER NOT NULL)");
+
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (1, 1, 24, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (2, 10, 49, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (3, 50, 99999, 1)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (4, 1, 24, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (5, 10, 49, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (6, 50, 99999, 2)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (7, 1, 24, 3)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (8, 10, 49, 3)");
+     db.execSQL(" INSERT INTO rangosxtipocopiacolor VALUES (9, 50, 99999, 3)");
+
+     db.execSQL("CREATE TABLE ventas (\n" +
+             "  idVenta INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+             "  fechaVenta datetime NOT NULL,\n" +
+             "  cantidadCopiasTotal INTEGER NOT NULL,\n" +
+             "  Total NUMERIC NOT NULL)");
+
+ }catch (Exception ex)
+ {
+     Log.d("Error en " ,ex.getMessage());
+ }
+    }
 
 }
 
